9257ed35e3c8c088e138cec986a01164f472f3f6
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-05-12 00:35:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen flex justify-center flex-col items-center gap-4'>
+      <h1 className='text-3xl'>
+        The Page is Under Construction. Thanks for your patience!
+      </h1>
+      <h2 className='text-2xl'>Take love form GAAC Accounting Family!</h2>
+    </div>
+  )
+}
+
+export default App
